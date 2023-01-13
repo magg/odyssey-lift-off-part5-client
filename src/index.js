e13a186f0de63d1f124320907dde0c3c61f3e062
Miguel Gonzalez <maggonzz@gmail.com>
2023-01-12 21:56:26 -0700

    test remove

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-2c08.up.railway.app/", // change this to YOUR server URI
+  uri: "https://odyssey-lift-off-part5-server-production-2c08.up.railway.app/",
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
